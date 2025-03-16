7964309075968ea63e8e2599e245b9ec33d011d1
Samuel Pereira <samuelsantana778@gmail.com>
2025-03-16 10:51:42 -0300

    first commit

---
+++ communication-server/src/main/java/com/tom/service/comms/CommunicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.tom.service.comms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationServerApplication.class, args);
+	}
+
+}
---
+++ communication-server/src/test/java/com/tom/service/comms/CommunicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.service.comms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
